8cb1c91920b2d7e2ef3adfe6057ecd25e3206afc
Gourav Saxena <gourav.saxena@kaltura.com>
2021-03-18 15:30:47 +0530

    - Travis build

--- smartswitch/src/main/java/com/kaltura/playkit/plugins/smartswitch/pluginconfig/SmartSwitchParser.kt
+++ smartswitch/src/main/java/com/kaltura/playkit/plugins/smartswitch/pluginconfig/SmartSwitchParser.kt
@@ -29,12 +29,14 @@ class CDNList {
 
 class SmartSwitchErrorResponse {
     var messages: ArrayList<ErrorResponseBody>? = null
-    // var data: Array? = null //TODO
+    // TODO
+    // var data: Array? = null
 }
 
 class ErrorResponseBody {
     var type: String? = null
     var code: String? = null
     var message: String? = null
-    // var parameters: Array? = null //TODO
+    // TODO
+    // var parameters: Array? = null
 }
