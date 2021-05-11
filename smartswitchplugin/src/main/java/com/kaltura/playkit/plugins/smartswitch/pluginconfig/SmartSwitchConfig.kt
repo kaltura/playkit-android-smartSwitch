a23004ad8a6891b29c960417579965abbc114cc1
Gourav Saxena <gourav.saxena@kaltura.com>
2021-05-11 15:51:10 +0530

    - Added doc

--- smartswitchplugin/src/main/java/com/kaltura/playkit/plugins/smartswitch/pluginconfig/SmartSwitchConfig.kt
+++ smartswitchplugin/src/main/java/com/kaltura/playkit/plugins/smartswitch/pluginconfig/SmartSwitchConfig.kt
@@ -1,7 +1,20 @@
 package com.kaltura.playkit.plugins.smartswitch.pluginconfig
 
+import androidx.annotation.NonNull
 import java.util.HashMap
 
-data class SmartSwitchConfig @JvmOverloads constructor(val accountCode: String? = null,
-                                                       val originCode: String? = null,
+/**
+ * Create SmartSwitch Config object
+ *
+ * @param accountCode Mandatory: YOUBORA account code
+ * @param originCode Mandatory: CDN group configured to select a subset of configured CDNs
+ * @param optionalParams Optional:
+ *                          Ex. ip(String), extraData(Boolean), userAgent(String),
+ *                          live(Boolean), nva(Not Valid After timestamp in UTC +1, Integer),
+ *                          nvb(Not  Valid  Before  timestamp  in  UTC +1,Integer), token(String)
+ *
+ * @return SmartSwitchConfig
+ */
+data class SmartSwitchConfig @JvmOverloads constructor(@NonNull val accountCode: String? = null,
+                                                       @NonNull val originCode: String? = null,
                                                        val optionalParams: HashMap<String, String>? = null)
